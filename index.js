b9869d1e6073f77a320de862bf5994a1e7c13ec5
Mattia Rasulo <mattiarasulo@Mac-mini-di-Mattia.local>
2021-08-13 10:15:46 +0200

    Add error handling for unsupported extensions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/local/bin/node
 
 const { readdir } = require("fs");
 const path = require("path");
@@ -12,22 +12,40 @@ const targetDirectory = process.argv[2];
 async function resizePictures() {
   try {
     console.log(targetDirectory);
+    let fileName;
     readdir(targetDirectory, async (err, files) => {
       try {
         for (const file of files) {
-          //console.log(path.join(process.cwd(), targetDirectory, file));
-
           const processedFile = path.join(process.cwd(), targetDirectory, file);
-          const metadata = await sharp(processedFile).metadata();
-          const format =
-            metadata.width > metadata.height ? [1200, null] : [null, 1200];
+          fileName = file;
+          try {
+            const metadata = await sharp(processedFile).metadata();
 
-          await sharp(processedFile)
-            .rotate()
-            .resize(...format)
-            .toFormat("jpg")
-            .jpeg({ quality: 90 })
-            .toFile(path.join(process.cwd(), file));
+            const format =
+              metadata.width > metadata.height ? [1200, null] : [null, 1200];
+
+            await sharp(processedFile)
+              .rotate()
+              .resize(...format)
+              .toFormat("jpg")
+              .jpeg({ quality: 90 })
+              .toFile(path.join(process.cwd(), file));
+
+            console.log(chalk.green(`${file} correctly resized`));
+          } catch (err) {
+            console.log(err);
+            if (
+              err.message === "Input file contains unsupported image format"
+            ) {
+              console.warn(
+                `${chalk.bgYellow("WARN")} ${chalk.yellow(
+                  `The file extension is not supported. The file ${fileName} is being skipped`
+                )}`
+              );
+            } else {
+              console.error(chalk.red(err.message));
+            }
+          }
         }
       } catch (err) {
         console.error(chalk.red(err.message));
